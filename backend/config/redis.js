1be6e3f92516d9c4bc4e7720bc0773d82ff54349
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2026-01-21 14:58:25 +0530

    redis  reconfiguration

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -3,8 +3,8 @@ import 'dotenv/config';
 
 export const createRedisCOnnection = ()=> {
     return new Redis({
-            host: process.env.REDIS_HOST || "127.0.0.1",
-            port: Number(process.env.REDIS_PORT || 6379),
+            host: process.env.REDIS_HOST ,
+            port: Number(process.env.REDIS_PORT),
             password: process.env.REDIS_PASSWORD || undefined,
             maxRetriesPerRequest: null
         }
