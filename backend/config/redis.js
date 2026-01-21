1e09a6eb04bc09982e3e9a0ca69dc57f85d2ec0f
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2026-01-21 11:05:12 +0530

    redis connection

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,5 +1,11 @@
 import Redis from "ioredis";
 
-const redisConnection  = new Redis(6380);
-
-export default redisConnection;
+export const creatRedisCOnnection = ()=> {
+    return new Redis({
+            host: process.env.REDIS_HOST || "127.0.0.1",
+            port: Number(process.env.REDIS_PORT || 6379),
+            password: process.env.REDIS_PASSWORD || undefined,
+            maxRetriesPerRequest: null
+        }
+    )
+};
