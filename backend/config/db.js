925911813e09c72d9e8d6a7fab91de67723c8f41
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2026-01-22 15:56:55 +0530

    Hosted postgreSQl

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,18 @@ import 'dotenv/config'
 
 
 const {Pool} = pg;
-
+//for Local host
+// export const db = new Pool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+// }
+// )
 export const db = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-}
-)
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
