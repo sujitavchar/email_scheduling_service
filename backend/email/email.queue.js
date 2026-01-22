e48479769e8f8537e49f836774ebb4988269f4f3
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2026-01-22 18:41:26 +0530

    Servers restarts survival

--- backend/email/email.queue.js
+++ backend/email/email.queue.js
@@ -38,7 +38,7 @@ async function addEmailJob(data) {
 //Adds 'scheduled','failed', 'processing' jobs again in queue maintaining idempotency
 async function resumeEmails() {
   const result = await db.query(
-    `SELECT * FROM emails WHERE status IN ('failed')`
+    `SELECT * FROM emails WHERE status IN ('failed','scheduled','processing')`
   );
 
   for (const email of result.rows) {
