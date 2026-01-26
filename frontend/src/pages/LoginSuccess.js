90078ae542535dbd6f9f91f753443f2397fb1069
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2026-01-26 22:56:20 +0530

    import bugs fixed

--- frontend/src/pages/LoginSuccess.js
+++ frontend/src/pages/LoginSuccess.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import jwtDecode from "jwt-decode";
+import {jwtDecode} from "jwt-decode";
 
 const LoginSuccess = () => {
   const { setUser } = useAuth();
